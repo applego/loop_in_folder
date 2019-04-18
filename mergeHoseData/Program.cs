60213bbb9faad7b849360d299fadd4525955a139
applego <applegorillappa@gmail.com>
2019-04-18 17:21:53 +0900

    first comit

---
+++ mergeHoseData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace mergeHoseData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//指定したキーの値を取得
+			//見つからないときはnullを返す
+			Console.WriteLine(System.Configuration.ConfigurationManager.AppSettings["Comment"]);
+
+			//すべてのキーとその値を取得
+			foreach (string key in System.Configuration.ConfigurationManager.AppSettings.AllKeys)
+			{
+				Console.WriteLine("{0} : {1}",
+					key, System.Configuration.ConfigurationManager.AppSettings[key]);
+			}
+		}
+	}
+}
